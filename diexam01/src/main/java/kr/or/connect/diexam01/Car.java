cf57a5c735588467ff3f52c7e64eface9684225c
ben <shb0801@naver.com>
2023-07-23 00:23:16 +0900

    07-23 Java Config 완료

--- diexam01/src/main/java/kr/or/connect/diexam01/Car.java
+++ diexam01/src/main/java/kr/or/connect/diexam01/Car.java
@@ -1,15 +1,17 @@
 package kr.or.connect.diexam01;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Car {
+	@Autowired
 	private Engine v8;
 	
 	public Car() {
 		System.out.println("Car 생성자");
 	};
 	
-	public void setEngine(Engine e) {
-		this.v8 = e;
-	}
 	
 	public void run() {
 		System.out.println("엔진을 이용하여 달립니다.");
@@ -19,7 +21,6 @@ public class Car {
 	public static void main(String[] args) {
 		Engine e = new Engine();
 		Car c = new Car();
-		c.setEngine(e);
 		c.run();
 	}
 }
