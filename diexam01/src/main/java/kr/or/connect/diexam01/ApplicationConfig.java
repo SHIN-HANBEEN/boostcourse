f80eb4391c36650f2c2dfb53eb2943be17ac34f8
ben <shb0801@naver.com>
2023-07-26 21:31:24 +0900

    07-26 Spring JDBC 3 완료 commit

--- diexam01/src/main/java/kr/or/connect/diexam01/ApplicationConfig.java
+++ diexam01/src/main/java/kr/or/connect/diexam01/ApplicationConfig.java
@@ -3,7 +3,7 @@ package kr.or.connect.diexam01;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+/@Configuration
 public class ApplicationConfig {
 	@Bean
 	public Car car(Engine e) {
